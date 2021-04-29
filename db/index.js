7e50d8825ba01f5be5705886bb3e9fb4b135b901
christiangerbig <chr_gerbig@web.de>
2021-04-29 10:39:45 +0200

    new deployment

--- db/index.js
+++ db/index.js
@@ -5,8 +5,8 @@ const mongoose = require("mongoose");
 // ℹ️ Sets the MongoDB URI for our app to have access to it.
 // If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
 
-// const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/Trackertools";
-const MONGO_URI = process.env.MONGODB_URI || "mongodb+srv://Christian:amiga#1forever@cluster0.hnbt9.mongodb.net/trackertools?retryWrites=true&w=majority";
+const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/Trackertools";
+//const MONGO_URI = process.env.MONGODB_URI || "mongodb+srv://Christian:amiga#1forever@cluster0.hnbt9.mongodb.net/trackertools?retryWrites=true&w=majority";
 
 mongoose.connect(
   MONGO_URI, 
