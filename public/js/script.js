3b3c1a9bba3cc3e78b7219a6a69427d5e2743a6f
christiangerbig <chr_gerbig@web.de>
2021-05-07 09:47:15 +0200

    audio sample play for github link added

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,36 @@
 // Handler for main part
 const handleMainPart = () => {
+  
+  // Load cat meow sound
+  const loadCatMeowSound = () => {
+
+    // Handler for load cat meow sound
+    const handleCatMeowSoundLoad = () => {
+      catMeowSound.removeEventListener(
+        "load",
+        handleCatMeowSoundLoad
+      );
+    }
+    // Add handler for load cat meow sound
+    const catMeowSound = new Audio("./sounds/cute-cat-meow.mp3");
+    catMeowSound.addEventListener(
+      "load",
+      handleCatMeowSoundLoad
+    );
+    return catMeowSound;
+  }
+  const catMeowSound = loadCatMeowSound();
+
+  // Handler for playing cat meow sound
+  const handlePlayCatMeowSound = () => {
+    catMeowSound.play();
+  }
+  // Add handler for playing cat meow sound
+  document.querySelector("#gitHubLink").addEventListener(
+    "click",
+    handlePlayCatMeowSound
+  );
+  
 }
 // Add handler for main part
 document.addEventListener(
